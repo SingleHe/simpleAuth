6db72f50b6b0d2b0710e0c5145e3571c808aa4d9
SingleHe1990 <LargerStudet@gmail.com>
2018-01-16 20:41:56 +0800

    update local PasswordHelper.java

--- src/main/java/com/singlehe/shiro/service/impl/PasswordHelper.java
+++ src/main/java/com/singlehe/shiro/service/impl/PasswordHelper.java
@@ -36,6 +36,5 @@ public class PasswordHelper {
                 ByteSource.Util.bytes(user.getCredentialsSalt()),
                 hashIterations).toHex();
         user.setPassword(newPassword);
-
     }
 }
